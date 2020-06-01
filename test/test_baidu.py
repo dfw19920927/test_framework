f9e4b2a35b2e6644c3d4328111d00f75424c4821
dfw <farrell_Tung@hotmail.com>
2020-06-01 23:39:24 +0800

    Y

--- test/test_baidu.py
+++ test/test_baidu.py
@@ -3,8 +3,9 @@ import time
 import unittest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from utils.config import Config, DRIVER_PATH
+from utils.config import Config, DRIVER_PATH, DATA_PATH
 from utils.log import logger
+from utils.file_reader import ExcelReader
 
 
 class testBaidu(unittest.TestCase):
@@ -12,33 +13,54 @@ class testBaidu(unittest.TestCase):
     # base_path = os.path.dirname(os.path.abspath(__file__))+'\..'
     # driver_path = os.path.abspath(base_path+'\drivers\chromedriver.exe')
     URL = Config().get('URL')
+    excel = DATA_PATH + '/baidu.xlsx'
     locator_kw = (By.ID, 'kw')
     locator_su = (By.ID, 'su')
     locator_result = (By.XPATH, '//div[contains(@class, "result")]/h3/a')
 
-    def setUp(self):
+    def sub_setUp(self):
         self.driver = webdriver.Chrome(executable_path=DRIVER_PATH+'\chromedriver.exe')
         self.driver.get(self.URL)
 
-    def tearDown(self):
+    def sub_tearDown(self):
         self.driver.quit()
 
-    def test_search_0(self):
-        self.driver.find_element(*self.locator_kw).send_keys('selenium 灰蓝')
-        self.driver.find_element(*self.locator_su).click()
-        time.sleep(2)
-        links = self.driver.find_elements(*self.locator_result)
-        for link in links:
-            logger.info(link.text)
+    # def setUp(self):
+    #     self.driver = webdriver.Chrome(executable_path=DRIVER_PATH+'\chromedriver.exe')
+    #     self.driver.get(self.URL)
+    #
+    # def tearDown(self):
+    #     self.driver.quit()
 
-    def test_search_1(self):
-        self.driver.find_element(*self.locator_kw).send_keys('python selenium')
-        self.driver.find_element(*self.locator_su).click()
-        time.sleep(2)
-        links = self.driver.find_elements(*self.locator_result)
-        for link in links:
-            logger.info(link.text)
+    def test_search(self):
+        datas = ExcelReader(self.excel).data
+        for d in datas:
+            with self.subTest(data=d):
+                self.sub_setUp()
+                self.driver.find_element(*self.locator_kw).send_keys(d['search'])
+                self.driver.find_element(*self.locator_su).click()
+                time.sleep(2)
+                links = self.driver.find_elements(*self.locator_result)
+                for link in links:
+                    logger.info(link.text)
+                self.sub_tearDown()
+
+    # def test_search_0(self):
+    #     self.driver.find_element(*self.locator_kw).send_keys('selenium 灰蓝')
+    #     self.driver.find_element(*self.locator_su).click()
+    #     time.sleep(2)
+    #     links = self.driver.find_elements(*self.locator_result)
+    #     for link in links:
+    #         logger.info(link.text)
+    #
+    # def test_search_1(self):
+    #     self.driver.find_element(*self.locator_kw).send_keys('python selenium')
+    #     self.driver.find_element(*self.locator_su).click()
+    #     time.sleep(2)
+    #     links = self.driver.find_elements(*self.locator_result)
+    #     for link in links:
+    #         logger.info(link.text)
 
 
 if __name__ == '__main__':
-    unittest.main()
+    unittest.main(verbosity=2)
--- utils/file_reader.py
+++ utils/file_reader.py
@@ -17,4 +17,64 @@ class YamlReader:
             with open(self.yamlf, 'rb') as f:
                 self._data = list(yaml.safe_load_all(f))
         return self._data
-    
+
+
+class SheetTypeError(Exception):
+    pass
+
+
+class ExcelReader:
+    """
+    读取excel文件中的内容，返回list
+    如excel文件中的内容为
+    |A|B|C|
+    |A1|B1|C1|
+    |A2|B2|C2|
+    如果print（ExcelReader（excel，title_line=True）.data）输出结果：
+    [{A: A1, B: B1, C:C1}, {A:A2, B:B2, C:C2}]
+
+    如果 print(ExcelReader(excel, title_line=False).data)，输出结果：
+    [[A,B,C], [A1,B1,C1], [A2,B2,C2]]
+
+    可以指定sheet，通过index或者name：
+    ExcelReader(excel, sheet=2)
+    ExcelReader(excel, sheet='BaiDuTest')
+    """
+    def __init__(self, excel, sheet=0, title_line=True):
+        if os.path.exists(excel):
+            self.excel = excel
+        else:
+            raise FileNotFoundError('文件不存在！')
+        self.sheet = sheet
+        self.title_line = title_line
+        self._data = list()
+
+    @property
+    def data(self):
+        if not self._data:
+            workbook = open_workbook(self.excel)
+            if type(self.sheet) not in [int, str]:
+                raise SheetTypeError('Please pass in <type int> or <type str>,not{0}'.format(type(self.sheet)))
+            elif type(self.sheet) == int:
+                s = workbook.sheet_by_index(self.sheet)
+            else:
+                s = workbook.sheet_by_name(self.sheet)
+
+            if self.title_line:
+                title = s.row_values(0)
+                for col in range(1, s.nrows):
+                    self._data.append(dict(zip(title, s.row_values(col))))
+            else:
+                for col in range(0, s.nrows):
+                    self._data.append(s.row_values(col))
+        return self._data
+
+
+if __name__=='__main__':
+    y = 'D:\DFWworkspace\test_framework\config\config.yml'
+    reader = YamlReader(y)
+    print(reader.data)
+
+    e = 'D:\DFWworkspace\test_framework\data\baidu.xlsx'
+    reader = ExcelReader(e, title_line=True)
+    print(reader.data)
